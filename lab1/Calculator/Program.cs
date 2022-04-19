d9c078539e76f3999fe2336753264ae7a142ed80
maria <79695093+guzhiwo@users.noreply.github.com>
2022-04-19 15:48:44 +0300

    simple console calculator

---
+++ lab1/Calculator/Program.cs
@@ -0,0 +1,47 @@
+﻿float first;
+float second;
+float result;
+
+Console.Write("Input first number: ");
+
+if (!float.TryParse(Console.ReadLine(), out first)) //out значит что аргумент передается по ссылке и будет изменен этой функцией
+{
+    Console.WriteLine("Whoops, incorrect input");
+    return 1;
+}
+
+Console.Write("Input second number: ");
+
+if (!float.TryParse(Console.ReadLine(), out second))
+{
+    Console.WriteLine("Whoops, incorrect input");
+    return 1;
+}
+
+Console.Write("Input operation (+, -, *, /, ^): ");
+
+switch(Console.ReadLine())
+{
+    case "+":
+        result = first + second; 
+        break;
+    case "-":
+        result = first - second; 
+        break;
+    case "*":
+        result = first * second; 
+        break;
+    case "/":
+        result = first / second; 
+        break;
+    case "^":
+        result = (float)Math.Pow(first, second);
+        break;
+    default:
+        Console.WriteLine("Whoops, incorrect input"); 
+        return 1;
+}
+
+Console.WriteLine("Result: " + result);
+
+return 0;

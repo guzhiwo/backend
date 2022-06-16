245257818282c8a1c2a49078478c16e2ea268f08
maria <79695093+guzhiwo@users.noreply.github.com>
2022-06-16 23:04:20 +0300

    Add files via upload

---
+++ lab2/TaskBoardAPI_DB/DTO/ColumnDTO.cs
@@ -0,0 +1,18 @@
+﻿using ScramBoardAPI.Models;
+
+namespace ScramBoardAPI.DTO
+{
+    public class ColumnDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<TaskDTO> Tasks { get; set; }
+         
+        public ColumnDTO(Column column)
+        {
+            Id = column.UnicalID;
+            Name = column.ColumnName;
+            Tasks = column._Tasks.Select(t => new TaskDTO(t)).ToList();
+        }
+    }
+}

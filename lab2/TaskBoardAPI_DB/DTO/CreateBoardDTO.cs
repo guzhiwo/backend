836fd28a425c821748e28e1b48faf0171a5f6fa5
maria <79695093+guzhiwo@users.noreply.github.com>
2022-06-16 23:30:53 +0300

    Delete lab2/TaskBoardAPI_DB directory

--- lab2/TaskBoardAPI_DB/DTO/CreateBoardDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ScramBoardAPI.DTO
-{
-    public class CreateBoardDTO
-    {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-    }
-}
- 
--- lab2/TaskBoardAPI_DB/DTO/CreateColumnDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ScramBoardAPI.DTO
-{
-    public class CreateColumnDTO
-    {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-    }
-}
- 
